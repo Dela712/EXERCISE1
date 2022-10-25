da02d38f199a6b9d183ac49caad46994a854d078
dela712 <dfitrianik17@gmail.com>
2022-10-24 17:11:14 -0700

    Revisi

---
+++ src/EX 2.kt
@@ -0,0 +1,7 @@
+fun main() {
+
+    val number = arrayOf("2", "4", "6")
+    for (i in 1 .. 100){
+        println("Value is $i")
+    }
+}
---
+++ src/EX 3.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+    val score1 = 60
+    val score2 = 70
+    val score3 = 80
+    val score4 = 90
+
+    val scoreStatus = if (score4 >= score1) {
+        "Selamat! Anda mendapatkan nilai A"
+   } else if (score2 >= score1) {
+        "Selamat! Anda mendapatkan nilai B"
+   } else if (score2 >= score1) {
+        "Selamat! Anda mendapatkan nilai C"
+}
